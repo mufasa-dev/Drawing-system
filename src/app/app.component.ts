26e1deb4565cb2c730778f4be43eb51b4587fa7c
mufasa-dev <bruno.ribeiro96@hotmail.com.br>
2025-03-31 13:01:07 -0300

    add canva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'drawing-system';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') canva!: ElementRef<HTMLCanvasElement>;
+  private drawing: boolean = false;
+  public ctx!: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    let ctx = this.canva.nativeElement.getContext('2d');
+    if (ctx) this.ctx = ctx;
+  }
+
+  startDrawing(event: MouseEvent) {
+    this.drawing = true;
+    this.ctx.beginPath;
+    this.ctx.moveTo(event.offsetX, event.offsetY);
+  }
+
+  stopDrawing() {
+    this.drawing = false;
+  }
+
+  draw(event: MouseEvent) {
+    if (!this.drawing) return;
+    this.ctx.lineTo(event.offsetX, event.offsetY);
+    this.ctx.stroke();
+  }
+
+  changeColor(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.ctx.strokeStyle = input.value;
+  }
 }
