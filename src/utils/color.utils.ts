05d4ad57738f53d154827d9467b8b1924cdf25b2
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-10 12:20:32 -0300

    feat: add hhex to rgb function

--- src/utils/color.utils.ts
+++ src/utils/color.utils.ts
@@ -9,3 +9,13 @@ export function rgbaToHex(r: number, g: number, b: number): string {
         .join('')
     );
 }
+
+export function hexToRgb(hex: string): [number, number, number] {
+  hex = hex.replace(/^#/, '');
+  const bigint = parseInt(hex, 16);
+  return [
+    (bigint >> 16) & 255,
+    (bigint >> 8) & 255,
+    bigint & 255
+  ];
+}
