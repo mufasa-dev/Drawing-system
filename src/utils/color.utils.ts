df3bb4307b2e31fdf69d8c57298e58beae6c473c
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-07-14 11:24:12 -0300

    feat: create function rgb to Hsv

--- src/utils/color.utils.ts
+++ src/utils/color.utils.ts
@@ -40,3 +40,25 @@ export function hexToRgb(hex: string): [number, number, number] {
     bigint & 255
   ];
 }
+
+export function rgbToHsv(r: number, g: number, b: number): [number, number, number] {
+  r /= 255; g /= 255; b /= 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  const delta = max - min;
+
+  let h = 0;
+  if (delta !== 0) {
+    if (max === r) h = 60 * (((g - b) / delta) % 6);
+    else if (max === g) h = 60 * (((b - r) / delta) + 2);
+    else h = 60 * (((r - g) / delta) + 4);
+  }
+
+  if (h < 0) h += 360;
+
+  const s = max === 0 ? 0 : delta / max;
+  const v = max;
+
+  return [h, s, v];
+}
